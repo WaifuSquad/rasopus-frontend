a14fb6e895372d6e5fa6e01ad13878d37ad6eb62
Nguyen <minhoan89@gmail.com>
2025-01-12 16:35:05 +0000

    Vue, Tailwind, Devtools, Shadcn, Setup Page, Routing, OAS dependency

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/setup',
-      name: 'setup',
+      path: "/setup",
+      name: "setup",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/SetupView.vue"),
     },
   ],
-})
+});
 
-export default router
+//TODO: logic from backend when to show setup page
+const firstTime = true;
+router.beforeEach((to, from, next) => {
+  if (firstTime && to.name !== "setup") {
+    return next({ name: "setup" });
+  }
+  next();
+});
+
+export default router;
