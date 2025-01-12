a637268a60abcbd98728935d73b0e4646d8af159
Minngus <62205137+Minngus@users.noreply.github.com>
2025-01-12 22:54:51 +0000

    Feature/waifu 15/basic setup and setup page (#4)
    * Vue, Tailwind, Devtools, Shadcn, Setup Page, Routing, OAS dependency
    
    * update oas json and generate api client

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -10,38 +9,65 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import Toaster from '@/components/ui/toast/Toaster.vue'
+import { useColorMode } from '@vueuse/core'
+import { FlFilledDarkTheme } from '@kalimahapps/vue-icons';
+const mode = useColorMode()
 </script>
 
 <template>
+  <Toaster />
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      test
-      <Button>Shadcn Test</Button>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <Button>Shadcn Test</Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button variant="outline">
+          <FlFilledDarkTheme
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <FlFilledDarkTheme
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem @click="mode = 'light'">
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'dark'">
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'auto'">
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </header>
-
-  <RouterView />
+  <main class="center-content-grid">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
+.center-content-grid {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
---
+++ src/views/SetupView.vue
@@ -0,0 +1,81 @@
+<script setup lang='ts'>
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+import { useForm } from 'vee-validate'
+import { useToast } from '@/components/ui/toast/use-toast'
+import { h } from 'vue'
+// import { DefaultApiFp } from '@/api'
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+const { toast } = useToast()
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(3),
+}))
+
+// api call
+// const { indexIndex } = DefaultApiFp()
+// indexIndex();
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = handleSubmit((values) => {
+  toast({
+    title: 'You submitted the following values:',
+    description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
+  })
+})
+</script>
+
+<template>
+  <Card class="w-[350px]">
+    <form v-on:submit="onSubmit">
+      <CardHeader>
+        <CardTitle>First Setup</CardTitle>
+        <CardDescription>Create admin account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <FormField v-slot="{ componentField }" name="username">
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="username" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField v-slot="{ componentField }" name="password">
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input type="password" placeholder="password" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </CardContent>
+      <CardFooter class="flex justify-between px-6 pb-6">
+        <Button type="submit">Create</Button>
+      </CardFooter>
+    </form>
+  </Card>
+</template>
